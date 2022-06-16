754184fa723e3e438846180266fa90856e48b210
charlesharris3 <c.harris3@ymail.com>
2022-06-16 11:36:16 -0700

    Item controller updated with API endpoints: getItemByName, getItemById, getItemQuantity; all 3 working successfully

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Item;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("item")
 public class ItemController {
 
-    @GetMapping("home") //http://localhost:8080/item/home
+    @GetMapping() //http://localhost:8080/item/
     public String home(){
         return "This is the item controller";
     }
+
+    //Return an item by its ID- using @PathVariable annotation
+    @GetMapping("/getItemById/{itemId}") //http://localhost:8080/item/getItemById
+    public Item getItemById(@PathVariable("itemId")int itemId){
+        System.out.println("Retrieving item by id: "+ itemId);
+        Item item = new Item(itemId,1,10,"Toilet Paper");
+        return item;
+    }
+
+    //Return an item by its name - using @PathVariable annotation
+    @GetMapping("/getItemByName/{itemName}") //http://localhost:8080/item/getItemByName
+    public Item getItemByName(@PathVariable("itemName")String itemName){
+        System.out.println("Retrieving item: "+ itemName);
+        Item item = new Item(100,1,10,itemName);
+        return item;
+    }
+
+    //Return number of items currently in stock
+    @GetMapping("/getItemQuantity/{itemName}") //http://localhost:8080/item/getItemQuantity
+    public Item getItemQuantity(@PathVariable("itemName")String itemName){
+        int totalItemQuanity = 1000000; //1 million items
+        System.out.println("The total number of items for: "+ itemName+" is "+ totalItemQuanity);
+        Item item = new Item(100, totalItemQuanity,10,itemName);
+        return item;
+    }
+
 }
