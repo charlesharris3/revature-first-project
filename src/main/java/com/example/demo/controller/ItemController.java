7e4322d65cc52aadc6404a93f38a3513401c142a
charlesharris3 <c.harris3@ymail.com>
2022-06-16 14:34:41 -0700

    Added AppConfig file; added comments to ItemController.java; added Item bean to ItemController.java

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("item")
 public class ItemController {
 
+    @Autowired() //Injects Item class into Spring - this is a Bean
+    Item item;
+
     @GetMapping("/home") //http://localhost:8080/item/home
     public String home(){
-        return "This is the item controller";
+        return "This is the item controller: " + item;
     }
 
     @GetMapping("/getAllItems") //http://localhost:8080/item/getAllItems
@@ -18,16 +22,16 @@ public class ItemController {
     }
 
     //Create a new product using the POST HTTP method
-    @PostMapping("/addItem")
+    @PostMapping("/addItem") //http://localhost:8080/item/addItem
     public String addItem(@RequestBody Item item){
-        System.out.println("Adding new item: "+ item);
+        System.out.println("Adding new item: "+ item.getItemName());
         return "Successfully saved item "+ item;
     }
 
     //Return an item by its ID- using @PathVariable annotation
     @GetMapping("/getItemById/{itemId}") //http://localhost:8080/item/getItemById
     public Item getItemById(@PathVariable("itemId")int itemId){
-        System.out.println("Retrieving item by id: "+ itemId);
+        System.out.println("Retrieving item by id: "+ item.getItemId());
         Item item = new Item(itemId,1,10,"Toilet Paper");
         return item;
     }
@@ -35,22 +39,22 @@ public class ItemController {
     //Return an item by its name - using @PathVariable annotation
     @GetMapping("/getItemByName/{itemName}") //http://localhost:8080/item/getItemByName
     public Item getItemByName(@PathVariable("itemName")String itemName){
-        System.out.println("Retrieving item: "+ itemName);
+        System.out.println("Retrieving item: "+ item.getItemName());
         Item item = new Item(100,1,10,itemName);
         return item;
     }
 
     //Update a product using the PUT HTTP method
-    @PutMapping("/updateItem")
+    @PutMapping("/updateItem") //http://localhost:8080/item/updateItem
     public String updateItem(@RequestBody Item item){
-        System.out.println("Updating item: "+ item);
+        System.out.println("Updating item: "+ item.getItemName());
         return "Successfully updated item "+ item;
     }
 
     //Delete an item by its ID - using @PathVariable: @DeleteMapping
     @DeleteMapping("/removeItemById/{itemId}") //http://localhost:8080/item/removeItemById
     public String deleteItemById(@PathVariable("itemId")int itemId){
-        Item item = new Item(itemId,1,10,"Toilet Paper");
+        Item item = new Item(itemId,100,10,"Item Name");
         return "Removing item by id: "+ itemId;
     }
 
@@ -59,7 +63,7 @@ public class ItemController {
     @GetMapping("/getItemQuantity/{itemName}") //http://localhost:8080/item/getItemQuantity
     public String getItemQuantity(@PathVariable("itemName")String itemName){
         int totalItemQuantity = 1000000; //1 million items
-        Item item = new Item(100, totalItemQuantity,10,itemName);
+        Item item = new Item(1, totalItemQuantity,10,itemName);
         return "The total quantity of item "+ item.getItemName() +" is "+ item.getTotalItemQuantity();
     }
 
