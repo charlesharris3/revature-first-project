975c9f59d0c0be10c10a4e61f8d8c521073ac083
charlesharris3 <c.harris3@ymail.com>
2022-06-19 02:31:57 -0700

    Configured database to app in application.properties; created JPA Entity for Item.java

--- src/main/java/com/example/demo/model/Item.java
+++ src/main/java/com/example/demo/model/Item.java
@@ -5,11 +5,18 @@ import lombok.Data; //lombok library for standard class methods
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component; //creates Bean
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Component //used to generate this class as a Bean
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "items", schema = "public")
 public class Item {
+    @Id
     private int itemId;
     private int totalItemQuantity;
     private int itemPrice;
