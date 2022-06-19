39f089c0030709ba5785ec1712e9826e262c6b90
charlesharris3 <c.harris3@ymail.com>
2022-06-19 00:08:38 -0700

    Removed 'AppConfig' file; added @Component annotation in 'Item.java'

--- src/main/java/com/example/demo/model/Item.java
+++ src/main/java/com/example/demo/model/Item.java
@@ -3,7 +3,9 @@ package com.example.demo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data; //lombok library for standard class methods
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component; //creates Bean
 
+@Component //used to generate this class as a Bean
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
